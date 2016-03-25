2f2cd30bc8cf22af61482a7e347d536c6420414a
xaadim <khadimbacke@gmail.com>
2016-03-25 21:24:51 +0100

    traduction page de connexion

--- carpooling/language/french/login_lang.php
+++ carpooling/language/french/login_lang.php
@@ -5,9 +5,11 @@ Langue du tableau de bord Administrateur
 ******************************************/
 
 $lang['error_authentication_failed']	= "Erreur d'authentication !";
+$lang['authentication_success']			= "Connexion reussie, pentientez !";
 $lang['message_logged_out']				= 'Vous vous êtes déconnecté';
-$lang['Carpooling_login']					= 'Login';
-$lang['login']							= 'Login';
+$lang['Carpooling_login']				= 'Connexion';
+$lang['login']							= 'Connexion';
 $lang['password']						= 'Mot de passe';
+$lang['forgot_password']				= 'Mot de passe oublié';
 $lang['email']							= 'Email';
 $lang['stay_logged_in']					= 'Rester connecté';
