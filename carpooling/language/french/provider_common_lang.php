4a5c61681cddcb51b4083d61951ec5a67c6848dd
llaff <fallfallo@hotmail.fr>
2016-03-26 21:10:53 +0100

    traduction provider_common_lang.php fran??ais

--- carpooling/language/french/provider_common_lang.php
+++ carpooling/language/french/provider_common_lang.php
@@ -5,13 +5,13 @@ Admin Common Language
 ******************************************/
 
 //header
-$lang['common_sales']				= 'Sales';
+$lang['common_sales']				= 'Ventes';
 $lang['common_orders']				= 'Réservations';
 $lang['common_customers']			= 'Clients';
 $lang['common_groups']				= 'Groupes';
-$lang['common_reports']				= 'Reports';
-$lang['common_coupons']				= 'Coupons';
-$lang['common_giftcards']			= 'Gift Cards';
+$lang['common_reports']				= 'Rapports';
+$lang['common_coupons']				= 'Bon de réduction';
+$lang['common_giftcards']			= 'Cartes-cadeaux';
 $lang['common_catalog']				= 'Catalogue';
 $lang['common_categories']			= 'Catégories';
 $lang['common_products']			= 'Produits';
@@ -24,12 +24,12 @@ $lang['common_administrative']		= 'Administratif';
 $lang['common_settings']			= 'Paramètres';
 $lang['common_locations']			= 'Répertoire'; // Location
 $lang['common_administrators']		= 'Administrateurs';
-$lang['common_note']				= 'Note';
-$lang['common_alert']				= 'Alert';
+$lang['common_note']				= 'Remarque';
+$lang['common_alert']				= 'Alerte';
 $lang['common_log_out']				= 'Déconnexion';
 $lang['common_front_end']			= 'Front End';
 $lang['common_dashboard']			= 'Tableau de bord';
-$lang['common_home']				= 'Home';
+$lang['common_home']				= 'Accueil';
 $lang['common_actions']				= 'Actions';
 
 //buttons
@@ -37,4 +37,4 @@ $lang['save']						= 'Enregistrer';
 $lang['edit']						= 'Modifier';
 $lang['delete']						= 'Supprimer';
 $lang['search']						= 'Recherche';
-$lang['toggle_wysiwyg']				= 'Toggle WYSIWYG';
+$lang['toggle_wysiwyg']				= 'Toggle WYSIWYG'; // Je connais pas de mot français correspondant
